881ca46b346e75f708c4c874982eb094d4aaed90
Daniel Apostolov <d.kaloyanoff@gmail.com>
2024-08-05 00:20:05 +0300

    Fixed font colors

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,13 +3,13 @@ import { useState } from 'react'
 export default function Example() {
 
     return (
-        <header className="bg-white">
-            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        <header className="bg-gray-900">
+            <nav aria-label="Global" className=" mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
 
                 {/* <------> Logo <-----> */}
                 <div className="flex lg:flex-1">
                     <a href="/" className="-m-1.5 p-1.5">
-                        <span className=" text-lg float-right font-bold ps-2 pt-0">The Mystic Library</span>
+                        <span className=" text-lg float-right font-bold ps-2 pt-0 text-white">The Mystic Library</span>
                         <img alt="Logo" src="https://i.postimg.cc/N0Q0bhLP/logo.png" className="h-8 w-auto" />
                     </a>
                 </div>
@@ -18,28 +18,28 @@ export default function Example() {
                 <div className="hidden lg:flex lg:gap-x-12">
 
 
-                    <a href="/" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                    <a href="/" className="text-sm font-semibold hover:text-blue-800 leading-6 text-white">
                         Home
                     </a>
-                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-white">
                         Catalog
                     </a>
-                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-white">
                         About us
                     </a>
-                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-white">
                         Create
                     </a>
-                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-white">
                         Company
                     </a>
                 </div>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end space-x-4">
-                    <a href="/register" className="text-sm font-semibold leading-6 text-gray-900 ">
+                    <a href="/register" className="text-sm font-semibold leading-6 text-white ">
                         Log in
                     </a>
 
-                    <a href="/login" className="rounded-md  text-sm font-semibold leading-6 bg-blue-700">
+                    <a href="/login" className="rounded-md  text-sm font-semibold leading-6 text-white bg-blue-700">
                         Sign up 
                     </a>
                 </div>
