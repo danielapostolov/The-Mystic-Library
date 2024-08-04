3cfa92030c13b59ffcb22d3f308293e02c3aedea
Daniel Apostolov <d.kaloyanoff@gmail.com>
2024-08-04 19:25:32 +0300

    Add Header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+export default function Example() {
+
+    return (
+        <header className="bg-white">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+
+                {/* <------> Logo <-----> */}
+                <div className="flex lg:flex-1">
+                    <a href="/" className="-m-1.5 p-1.5">
+                        <span className=" text-lg float-right font-bold ps-2 pt-0">The Mystic Library</span>
+                        <img alt="Logo" src="https://i.postimg.cc/N0Q0bhLP/logo.png" className="h-8 w-auto" />
+                    </a>
+                </div>
+
+
+                <div className="hidden lg:flex lg:gap-x-12">
+
+
+                    <a href="/" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                        Home
+                    </a>
+                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                        Catalog
+                    </a>
+                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                        About us
+                    </a>
+                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                        Create
+                    </a>
+                    <a href="#" className="text-sm font-semibold hover:text-blue-800 leading-6 text-gray-900">
+                        Company
+                    </a>
+                </div>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end space-x-4">
+                    <a href="/register" className="text-sm font-semibold leading-6 text-gray-900 ">
+                        Log in
+                    </a>
+
+                    <a href="/login" className="rounded-md  text-sm font-semibold leading-6 bg-blue-700">
+                        Sign up 
+                    </a>
+                </div>
+            </nav>
+
+        </header>
+    )
+}
