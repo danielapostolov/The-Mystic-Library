4c5cced0359e430bbf8f14dbc44e7042cb34d5b8
Daniel Apostolov <d.kaloyanoff@gmail.com>
2024-08-05 17:28:30 +0300

    Add images

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (<>
 
 
-        <div className="bg-gray-900">
+        <div className="">
             <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
                 <div className="relative isolate overflow-hidden bg-gray-800 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
                     <svg
@@ -29,15 +30,15 @@ export default function Home() {
                         </p>
                          <span className='text-lg text-white'>– Napoleon Bonaparte</span> 
                         <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                            <a
-                                href="/catalog"
+                            <Link 
+                                to="/catalog"
                                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                             >
                                 Explore
-                            </a>
-                            <a href="/about" className="text-sm font-semibold leading-6 text-white">
+                            </Link>
+                            <Link  to="/about" className="text-sm font-semibold leading-6 text-white">
                                 About us <span aria-hidden="true">→</span>
-                            </a>
+                            </Link>
                         </div>
                     </div>
                     <div className="relative mt-16 h-80 lg:mt-8">
