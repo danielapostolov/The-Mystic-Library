4bd0fde9ea0a787ca85a33a78c9a4647bef54bb9
Daniel Apostolov <d.kaloyanoff@gmail.com>
2024-08-07 00:15:05 +0300

    Add conditional rendering

--- client/src/components/books-catalog/BooksCatalog.jsx
+++ client/src/components/books-catalog/BooksCatalog.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
+
 import * as BooksAPI from '../../api/books-api';
+
+
 import BookCatalogCard from './book-catalog-card/BookCatalogCard';
 
 
@@ -7,8 +10,10 @@ export default function BookCatalog() {
   const [books, setBooks] = useState([])
 
   useEffect(() => {
-    BooksAPI.getAll()
-      .then(result => setBooks(result));
+    (async () => {
+      const result = await BooksAPI.getAll();
+      setBooks(result);
+    })()
   }, [])
 
   const data = [
@@ -44,9 +49,13 @@ export default function BookCatalog() {
   return (
     <div className="max-w-6xl mx-auto py-8 px-4 sm:px-6 lg:px-8 ">
       <h2 className="text-3xl font-extrabold tracking-tight text-white">Book Catalog</h2>
-      <div className="mt-6 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {books.map((book) => <BookCatalogCard key={book._id} {...book}/>)}
+
+      {books.length > 0
+        ?<div className="mt-6 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {books.map((book) => <BookCatalogCard key={book._id} {...book} />)}
       </div>
+      :<h2 className="text-2xl font-semibold text-center pt-64">No articles yet</h2>
+      }
     </div>
   );
 }
