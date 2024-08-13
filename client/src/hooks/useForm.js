ef731cc2626e6ed5ac8dd03fb67399b563b60f9a
Daniel Apostolov <d.kaloyanoff@gmail.com>
2024-08-13 18:07:17 +0300

    Clear form after submit

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -12,7 +12,10 @@ export function useForm(initialValues, submitCallback) {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        submitCallback(values)
+
+        submitCallback(values);
+
+        setValues(initialValues);
     }
 
     return {
